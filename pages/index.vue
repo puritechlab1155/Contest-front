d6396914359ad32a5eae5a14700a6312753da9fa
Puritechlab <pub@puritechlab.com>
2025-08-04 17:05:59 +0900

    메인 페이지+심사페이지

--- pages/index.vue
+++ pages/index.vue
@@ -1,134 +1,86 @@
 <template>
-    <!-- 컨텐츠 섹션 -->
-    <main class="sub">
-      <!-- 타이틀 배경 -->
-      <div class="subTitle-box">        
-        <h1 class="subTitle">심사 및 시상</h1>
-      </div>
+  <main class="index">
+    <section class="sec01">
+      <div class="wrap01">
+        <div class="contest-info">
+          <div>
+            <h1 class="main-title" style="white-space: nowrap;">제1회 뉴스피릿 미술 공모전</h1>
+            <div class="title-point" style="font-size: 65px; font-weight: 600; margin-top: 20px; margin-bottom: 10px; line-height: 130%;"> 미래의 스타, <br/>&nbsp;꿈꾸는 지구 </div>
+            <!-- <p class="date">2025.09.15(월) ~ 10.15(수)</p> -->
+          </div>
+          <p class="date" style="font-size: 30px; white-space: nowrap;">2025.09.15<span style="font-size: 20px;" class="date">(월)</span> ~ 10.15<span style="font-size: 20px;" class="date">(수)</span></p>
+          <!-- <div class="info-grid">
+            <div class="info-item">
+              <h2 class="info-title">주제</h2>
+              <p class="info-text">“미래의 스타, 꿈꾸는 지구"</p>
+            </div>
+            <div class="info-item"> 
+              <h2 class="info-title">참가대상</h2>
+              <p class="info-text">초등부 / 중등부 / 고등부 / 대학부(대학원생 포함)</p>
+            </div>
+            <div class="info-item large">
+              <h2 class="info-title">참가 부문</h2>
+              <p class="info-text">자유 창작 부문(주제 해석 및 표현 형식 자유)</p>
+              <p class="sub">→ 주제를 창의적으로 해석하여, 자신만의 세계를 표현</p>
+            </div>
+          </div> -->
+          <button class="btn01 white">모집요강 더보기</button>
 
-      <div class="sub-radius">
-
-        <!-- 네비게이션 -->
-        <div class="subNav inner">
-          <span>홈</span>
-          <img src="../assets/img/common/navarrow.svg" alt="화살표">
-          <span>공모 요강</span>
-          <img src="../assets/img/common/navarrow.svg" alt="화살표">
-          <span class="pointColor01">심사 및 시상</span>
         </div>
+        <div class="image-wrapper">
+          <div class="left-wrap">
+            <div class="masked-image left"></div>
+          </div>
+          <div class="right-wrap">
+            <div class="masked-image right"></div>
+          </div>
+        </div>
+      </div>
+      <div class="wrap02">
+        <div class="info-wrapper">
 
-        <!-- 컨텐츠  -->
-        <section class="awards subContent inner">
-          <div class="desc">
-            <h2 class="pointColor02">심사기준</h2>
-            <p class="f-18 mt-20">
-              심사 기준은 단순한 기술 평가가 아닌 새로운 꿈과 상상력, 발상과 표현, 정체성 구현, 가치 창조적 실험 등을 통한 사회적 메시지와 문화적 확장성을 적극 반영합니다.
-            </p>
+          <div class="info-item">
+            <div class="info-text">
+              <p class="info-title">접수 기간</p>
+              <p class="info-date">2025. 09. 15(월)<br/>~ 10. 15(수)</p>
+            </div>
+            <svg class="arrow-icon" width="34" height="35" viewBox="0 0 34 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M28.334 17.5L14.1681 3.33337L5.66732 3.33337L16.9973 14.6641L19.8332 17.5L16.9973 20.336L5.66732 31.6667L14.1681 31.6667L28.334 17.5Z" fill="#B31C45"/>
+            </svg>
+          </div>
+  
+          <div class="info-item">
+            <div class="info-text">
+              <p class="info-title">심사 기간</p>
+              <p class="info-date">2025. 10. 16(목)<br/>~ 10. 31(금)</p>
+            </div>
+            <svg class="arrow-icon" width="34" height="35" viewBox="0 0 34 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M28.334 17.5L14.1681 3.33337L5.66732 3.33337L16.9973 14.6641L19.8332 17.5L16.9973 20.336L5.66732 31.6667L14.1681 31.6667L28.334 17.5Z" fill="#B31C45"/>
+            </svg>
+          </div>
+  
+          <div class="info-item">
+            <div class="info-text">
+              <p class="info-title">결과 발표</p>
+              <p class="info-date">2025. 11. 03(월)</p>
+            </div>
+            <svg class="arrow-icon" width="34" height="35" viewBox="0 0 34 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M28.334 17.5L14.1681 3.33337L5.66732 3.33337L16.9973 14.6641L19.8332 17.5L16.9973 20.336L5.66732 31.6667L14.1681 31.6667L28.334 17.5Z" fill="#B31C45"/>
+            </svg>
+          </div>
+  
+          <div class="info-item">
+            <div class="info-text">
+              <p class="info-title">전시 일정</p>
+              <p class="info-date">2025. 11. 26(수)<br/>~ 12. 04(목)</p>
+            </div>
           </div>
 
-          <div class="contents-container mt-70">
-            <div class="flex-container">
-              <div class="bgBox left">
-                <h3 class="bgBox-title">심사 항목</h3>
-                <table>              
-                  <tr>
-                    <th class="border-r" style="width: 50%;">심사 항목</th>
-                    <th style="width: 50%;">평가 내용</th>
-                  </tr>
-                  <tr>
-                    <td class="border-r">새로운 꿈과 상상력</td>
-                    <td>도전의식, 신선한 창의성</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">조화와 균형</td>
-                    <td>조형 감각, 비례,<br class="smob"/> 구도 완성도</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">재료의 이해와 <br class="smob"/>운용 기법</td>
-                    <td>물성 이해, 재료 표현 능력</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">발상과 표현</td>
-                    <td>발상과 표현</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">양식(스타일)의 <br class="smob"/>시대성</td>
-                    <td>현대적 형식, 동시대 시각</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">시대적 메시지</td>
-                    <td>사회적 코드, <br class="smob"/>현실적 감각 표현</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">정체성 구현</td>
-                    <td>한국성, 동양적 미감, <br class="smob"/>전통 재해석</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">가치 창조적 실험</td>
-                    <td>진정성, <br class="smob"/>공간의 예술적 세계 구현</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">완성도와 안정감</td>
-                    <td>통일감, 에너지 내적 일관성</td>
-                  </tr>
-
-                </table>
-              </div>
-              <div class="bgBox right">
-                <h3 class="bgBox-title">시상 내역</h3>
-                <table>              
-                  <tr>
-                    <th class="border-r">구분</th>
-                    <th class="border-r">선정</th>
-                    <th>상금</th>
-                  </tr>
-                  <tr>
-                    <td class="border-r">분야별 <br class="smob"/>대상</td>
-                    <td class="border-r">전체<br class="smob"/> 4명</td>
-                    <td>대학부<br class="smob"/>(대학원생 포함)<br class="smob"/> 100만원,<br/>
-                      중고등부 50만원,<br/>
-                      초등부 문화상품권</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">특별상</td>
-                    <td class="border-r">00명</td>
-                    <td>상장</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">분야별 <br class="smob"/>최우수상</td>
-                    <td class="border-r">전체<br class="smob"/> 4명</td>
-                    <td>상장</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">우수상</td>
-                    <td class="border-r">00명</td>
-                    <td>상장</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">그린상</td>
-                    <td class="border-r">00명</td>
-                    <td>상장</td>
-                  </tr>
-                  <tr>
-                    <td class="border-r">블루상</td>
-                    <td class="border-r">00명</td>
-                    <td>상장</td>
-                  </tr>
-                </table>
-              </div>
-            </div>
-            <div class="caution mt-30">
-              <ul>
-                <li>수상자는 결과 발표 당일 공지사항에서 확인 가능합니다.</li>
-              </ul>
-            </div>
-          </div>      
-        </section>
-
+        </div>
+        <p class="info-pink">❖ 대상 수상자 기사화 및 언론 브랜딩 지원</p>
       </div>
-
-
-    </main>
+    </section>
+  </main>
 </template>
 
 <script setup>
@@ -136,38 +88,257 @@
 
 <style scoped>
 
-.sub .bgBox table {
-  border: 1px solid #ACACAC;
-  background: #FEFEFE;
-  text-align: center;
-  border-collapse: collapse;
-  width: 100%;
+
+.index .sec01 {
+  max-width: 1440px;
+  margin: 0 auto;
+  padding-left: 20px;
 }
-.sub .bgBox table th {
-  background: #FFF5F5;
-  padding: 10px;
+.index .sec01 .wrap01 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1248px; /* wrap01의 너비는 1248px로 유지 */
+  margin-left: auto; /* 왼쪽 여백을 자동으로 채워 오른쪽으로 정렬 */
+}
+/* .index .sec01 .contest-info {
+  max-width: 430px;
+} */
+
+
+.index .sec01 .main-title {
+  font-size: 35px;
+  font-weight: bold;
+}
+
+.index .sec01 .date {
+  font-size: 24px;
+  font-weight: 600;
+  color: #B31C45;
+  margin-top: 8px;
+}
+
+.index .sec01 .info-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto auto;
+  gap: 24px;
+  margin-top: 40px;
+}
+
+.index .sec01 .info-item {
+  line-height: 150%;
+}
+.index .sec01 .info-title {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 6px;
+}
+
+.index .sec01 .info-text {
   font-size: 18px;
   font-weight: 400;
+  margin: 0;
 }
-.sub .bgBox table td {
-  border-top: 1px solid #ACACAC;
-  padding: 10px;
+
+.index .sec01 .info-item.large {
+  grid-column: 2 / 3;
+  grid-row: 1 / 3; 
+}
+
+.index .sec01 .info-item.large .sub {
   font-size: 16px;
+  color: #5D5D5D;
+  margin-top: 15px;
 }
-.sub .bgBox table .border-r {
-  border-right: 1px solid #ACACAC;
+
+.index .sec01 .btn01.white {
+  margin-top: 60px;
 }
-.sub .awards {
+
+.index .sec01 .image-wrapper {
+  position: relative;
+  width: 100%;
+  height: 500px;
+  display: flex;
+  justify-content: flex-end;
+  gap: 25px;
+  padding: 0 20px;
+  /* background: #f0f0f0; */
+  overflow: hidden;
+}
+
+.index .sec01 .masked-image {
+  background-size: cover;
+  background-position: center;
+  transition: clip-path 1s ease-in-out;
+}
+
+
+.index .sec01 .masked-image.left {
+  width: 450px;
+  height: 480px;
+  background-image: url('../assets/img/main-top01.png');
+  clip-path: inset(0 0 0 0);
+
+  opacity: 0;
+  transform: scale(1.1); /* 살짝 크게 시작 */
+  animation: fadeInZoom 2s ease-out forwards;
+}
+
+/* 오른쪽 이미지 */
+.index .sec01 .masked-image.right {
+  width: 210px;
+  height: 300px;
+  background-image: url('../assets/img/main-top02.png');
+  clip-path: inset(0 0 0 0);
+
+  opacity: 0;
+  transform: scale(1.1); /* 살짝 크게 시작 */
+  animation: fadeInZoom 2s ease-out forwards;
+}
+
+.index .sec01 .image-wrapper .left-wrap {
+  position: relative;
+  width: 450px;
+  height: 480px;
+  overflow: hidden;
+  animation: leftRoundIn 5s ease-in-out forwards;
+}
+
+.index .sec01 .image-wrapper .right-wrap {
+  position: relative;
+  width: 210px;
+  height: 300px;
+  overflow: hidden;
+  animation: rightRoundIn 5s ease-in-out forwards;
+}
+
+@keyframes leftRoundIn {
+  0% { border-bottom-left-radius: 200px; }
+  40% { border-bottom-left-radius: 400px; }
+  55% { border-bottom-left-radius: 300px; }
+  70% { border-bottom-left-radius: 350px; }
+  90% { border-bottom-left-radius: 250px; }
+  100% { border-bottom-left-radius: 250px; }
+}
+
+@keyframes rightRoundIn {
+  0%   { border-bottom-right-radius: 0; }
+  50%  { border-bottom-right-radius: 200px; } 
+  65%  { border-bottom-right-radius: 150px; } 
+  80%  { border-bottom-right-radius: 200px; }
+  100% { border-bottom-right-radius: 100px; }
+}
+
+@keyframes fadeInZoom {
+  0% {
+    opacity: 0;
+    transform: scale(2);
+  }
+  50% {
+    opacity: 0.7;
+    transform: scale(1);
+  }
+  80% {
+    opacity: 1.5;
+    transform: scale(1.2);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+
+.index .sec01 {
+  /* background-color: yellow; */
+}
+
+.index .sec01 .wrap02 {
+  width: 100vw;
+  margin-left: calc(-50vw + 50%);
+  padding: 0px 20px 40px;
+  background-image: url('../assets/img/main-sec01-bg.png');
+  background-repeat: no-repeat;   
+  background-size: cover;           
+  background-position: center center; 
+  margin-top: 80px;
+  text-align: center;
+}
+
+.index .sec01 .wrap02 .info-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 75px;
+  justify-content: center;
+}
+
+.index .sec01 .wrap02 .info-item {
+  display: flex;
+  align-items: center;
+  width: fit-content;
+}
+
+.index .sec01 .wrap02 .info-text {
+  width: 180px;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  height: 100px;
+}
+
+.index .sec01 .wrap02 .info-title {
+  font-weight: bold;
+  font-size: 24px;
+  color: #B31C45;
+}
+
+.index .sec01 .wrap02 .info-date {
+  font-size: 18px;
+  color: #5D5D5D;
+  font-weight: 400;
+  line-height: 150%;
+  margin-top: 10px;
+}
+
+.index .sec01 .wrap02 .info-pink {
+  color: #FF5972;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 150%;
+  margin-top: 10px;
+}
+
+.index .sec01 .wrap02 .arrow-icon {
+  margin-left: 10px;
+  transition: transform 0.3s ease;
+}
+
+
+
+@media (max-width: 768px) {
+  .index .sec01 .wrap02 .info-wrapper {
+    flex-direction: column;
+    gap: 30px;
+    align-items: flex-start;
+  }
+
+  .index .sec01 .wrap02 .info-item {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .index .sec01 .wrap02 .arrow-icon {
+    transform: rotate(90deg);
+    margin: 10px 0 0 0;
+  }
+
+  .index .sec01 .wrap02 .info-text {
+    width: 100%;
+    text-align: center;
+  }
 
 }
-@media (max-width: 768px) {
-  .sub .bgBox table td {
-    line-height: 1.2;
-    font-size: 14px;
-  }
-  .sub .bgBox table th {
-    font-size: 16px;
-  }
-}
+
 
 </style> 
