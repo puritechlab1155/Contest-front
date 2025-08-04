58df611b512c348fd3f0749fd6a3a882cbe338a6
Puritechlab <pub@puritechlab.com>
2025-08-04 17:20:15 +0900

    서브페이지 폴더

--- components/Header.vue
+++ components/Header.vue
@@ -364,7 +364,7 @@ onBeforeUnmount(() => {
   transform: translateY(-20px);
   transition: all 0.4s cubic-bezier(0.4, 0, 0.2, 1);
   z-index: 50;
-  height: 160px;
+  height: 180px;
 }
 
 .header .submenu-box.show {
---
+++ pages/guide/recruit.vue
@@ -0,0 +1,78 @@
+<template>
+  <!-- 컨텐츠 섹션 -->
+  <main class="sub">
+    <!-- 타이틀 배경 -->
+    <div class="subTitle-box">        
+      <h1 class="subTitle">모집요강</h1>
+    </div>
+
+    <div class="sub-radius">
+
+      <!-- 네비게이션 -->
+      <div class="subNav inner">
+        <span>홈</span>
+        <img src="../../assets/img/common/navarrow.svg" alt="화살표">
+        <span>공모 요강</span>
+        <img src="../../assets/img/common/navarrow.svg" alt="화살표">
+        <span class="pointColor01">모집요강</span>
+      </div>
+
+      <!-- 컨텐츠  -->
+      <section class="awards subContent inner">
+        <!-- <div class="desc">
+          <h2 class="pointColor02">심사기준</h2>
+          <p class="f-18 mt-20">
+            심사 기준은 단순한 기술 평가가 아닌 새로운 꿈과 상상력, 발상과 표현, 정체성 구현, 가치 창조적 실험 등을 통한 사회적 메시지와 문화적 확장성을 적극 반영합니다.
+          </p>
+        </div> -->
+
+        <div class="contents-container mt-70">
+        </div>      
+      </section>
+
+    </div>
+
+
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+.sub .bgBox table {
+border: 1px solid #ACACAC;
+background: #FEFEFE;
+text-align: center;
+border-collapse: collapse;
+width: 100%;
+}
+.sub .bgBox table th {
+background: #FFF5F5;
+padding: 10px;
+font-size: 18px;
+font-weight: 400;
+}
+.sub .bgBox table td {
+border-top: 1px solid #ACACAC;
+padding: 10px;
+font-size: 16px;
+}
+.sub .bgBox table .border-r {
+border-right: 1px solid #ACACAC;
+}
+.sub .awards {
+
+}
+@media (max-width: 768px) {
+.sub .bgBox table td {
+  line-height: 1.2;
+  font-size: 14px;
+}
+.sub .bgBox table th {
+  font-size: 16px;
+}
+}
+
+</style> 
